f74f162b4119d6a4d561f0273bee4fbb9aae26f2
AP <porwal.1@iitj.ac.in>
2020-10-20 11:45:49 +0530

    API used

--- Image Gallery/src/App.js
+++ Image Gallery/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ImageCard from "./components/cards";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=18777653-1cd1c5f1b75803c16892ce34e&q=${term}&image_type=photo&pretty=true`)
+      .then((res) => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setLoading(false);
+      })
+      .catch(err => console.log(err))
+  }, [term])
+
   return (
-    <div className="text-6xl">
-      <h1>Image Gallery</h1>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
