f214b4444c1e112b7cad6a63d4871911922f1190
AP <porwal.1@iitj.ac.in>
2020-10-28 14:38:24 +0530

    Loader Added SucessFully

--- Recipe App/src/components/HomeComponent.js
+++ Recipe App/src/components/HomeComponent.js
@@ -1,56 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import Recipe from "./Recipe";
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, CircularProgress } from '@material-ui/core';
 import SearchRecipe from "./SearchRecipe";
 
 const API_ID = "4f7e0920"
 const API_KEY = "93d11fb1afcd36f34ce1a4bb7b30deb3"
 
-const styles = makeStyles({
-    recipes: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexWrap: "wrap"
-    }
-})
 
 
 function Home() {
     const [recipes, setRecipes] = useState([]);
     const [query, setQuery] = useState('chicken');
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
-        getRecipe();
+        setLoading(true);
+        fetch(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`)
+            .then(res => res.json())
+            .then(data => {
+                setRecipes(data.hits);
+                setLoading(false);
+            })
+            .catch(err => { console.log(err) })
     }, [query])
 
     const search = (val) => {
         setQuery(val);
     }
 
-    const getRecipe = () => {
-        const example = `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`
-        fetch(example)
-            .then(res => res.json())
-            .then(data => {
-                setRecipes(data.hits)
-            })
-            .catch(err => { console.log(err) })
-    }
-
 
     return (
         <div>
             <SearchRecipe search={(val) => search(val)} />
-            <div>
-                <Box className={styles().recipes} >
-                    {recipes.map(recipe => (
-                        <Box key={recipe.recipe.calories} m={2}>
-                            <Recipe recipe={recipe.recipe} />
-                        </Box>
-                    ))}
+
+            {!isLoading && recipes.length === 0 &&
+                <Box display="flex" justifyContent="center">
+                    <h1>No Image Found..</h1>
                 </Box>
-            </div>
+            }
+
+            {isLoading ?
+                <Box display="flex" justifyContent="center" mt={5}>
+                    <CircularProgress />
+                </Box> :
+                <div>
+                    <Box display="flex" justifyContent="center" flexWrap="wrap" alignItems="center" >
+                        {recipes.map(recipe => (
+                            <Box key={recipe.recipe.calories} m={2}>
+                                <Recipe recipe={recipe.recipe} />
+                            </Box>
+                        ))}
+                    </Box>
+                </div>
+            }
         </div>
 
     )
